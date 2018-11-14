af30cbccc73a78c5e598105e6283807c5271f251
SergeyKozadaev <kozadaev@intaro.email>
2018-11-14 19:52:12 +0300

    pre-commit testing

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class ArticleController extends AbstractController
 {
@@ -18,16 +20,30 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}", name="app_news")
      */
-    public function news($slug) {
+    public function news($slug, LoggerInterface $logger, SessionInterface $session, Request $request) {
         $comments = [
             "first test comment",
             "second test comment #2",
             "3rd comment in test array"
         ];
 
+        $logger->info('news article controller');
+
+        $this->addFlash('notice', 'message string');
+
+        $session->set("name", "value");
+
+
+        if($slug == "error") {
+            throw $this->createNotFoundException("Not found exception");
+        }
+
+
         return $this->render('article/show.html.twig', [
             'title' => ucfirst($slug),
-            'comments' => $comments
+            'comments' => $comments,
+            'name' => $session->get("name", ""),
+            'request' => $request
         ]);
     }
 }
