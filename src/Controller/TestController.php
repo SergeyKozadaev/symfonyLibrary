4eeb6aa578ae4ee1f88090e6d7e0e9b3f873f03d
SergeyKozadaev <kozadaev@intaro.email>
2018-12-19 16:53:54 +0300

    testing php git hooks

---
+++ src/Controller/TestController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sergey
+ * Date: 19.12.18
+ * Time: 16:51
+ */
+
+namespace App\Controller;
+
+class TestController
+{
+    public function __construct()
+    {
+    }
+}
